12bd97adf13d7321c3c6a6046cc26db1f656af01
pshita <2374ya@gmail.com>
2026-09-17 00:14:00 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 // These values are safe to expose in client-side code; access is controlled
 // by Firestore security rules, not by hiding this object.
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID",
+  apiKey: "AIzaSyA7tRtwggT9wB6EfoerIEao5TbuU2Dxa3E",
+  authDomain: "point-3a9cf.firebaseapp.com",
+  projectId: "point-3a9cf",
+  storageBucket: "point-3a9cf.firebasestorage.app",
+  messagingSenderId: "953498896872",
+  appId: "1:953498896872:web:c8c5f7ad27f38b927a3c5d",
 };
 
 export const isFirebaseConfigured = !firebaseConfig.apiKey.startsWith("PASTE_");
